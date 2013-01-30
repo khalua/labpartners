7a90975201663099db2a58f06b9a671caf3ff732
Ryan Lee <rlee1121@gmail.com>
2013-01-30 15:39:40 -0500

    Added names and shuffle to main.rb

---
+++ main.rb
@@ -0,0 +1,5 @@
+names = "Adi Adrian Alex Anthony Ben Brian Bryan Chloe Eric Jane Jeff Jillian Joe Luke Matt Nick Nicky Ralph Ron Ryan Sandip Sean Shefali Simon Stephen".split(' ')
+
+puts names.shuffle
+
+

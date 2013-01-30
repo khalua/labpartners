0e8afac9249c9740aa5db01c3aed986d88a9a6c5
Ryan Lee <rlee1121@gmail.com>
2013-01-30 17:01:17 -0500

    Updated main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,41 @@
-names = "Adi Adrian Alex Anthony Ben Brian Bryan Chloe Eric Jane Jeff Jillian Joe Luke Matt Nick Nicky Ralph Ron Ryan Sandip Sean Shefali Simon Stephen".split(' ')
+names = "Adi Adrian Alex Anthony Ben Brian Bryan Chloe Eric Jane Jeff Jillian Joe Luke Matt Nick Nicky Ralph Ron Ryan Sandip Sean Shefali Simon Stephen".split(' ').shuffle
 
-puts names.shuffle
+print "Number of people per group: "
+size = gets.to_i
+
+if size < 1
+  print "Invalid number. Number of people per group: "
+  size = gets.to_i
+elsif size > 25
+  print "We don't have #{size} people in the class. Number of people per group: "
+  size = gets.to_i
+end
+
+puts ' '
+
+group_numb = 0
+
+while names.length > 0
+  a = names.take(size).to_a
+    if a.length == size
+      group_numb = group_numb + 1
+      puts "Group #{group_numb}:"
+      puts a
+      names = names - a
+      if names.length > size
+        puts ' '
+      else
+        nil
+      end
+
+    else
+      puts a
+      names = names - a
+      puts ' '
+    end
+end
+
+
+
 
 
